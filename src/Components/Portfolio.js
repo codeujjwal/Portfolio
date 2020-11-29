bcfcc8ad5e813418f980650cf035400f9d958c3c
ujjwal sharma <codeujjwal@users.noreply.github.com>
2020-11-30 01:02:53 +0530

    new

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -9,16 +9,11 @@ class Portfolio extends Component {
           <div key={projects.title} className="columns portfolio-item">
             <div className="item-wrap">
               <a href={projects.url} title={projects.title}>
+                <div className="portfolio-item-meta">
+                  <h5>{projects.title}</h5>
+                  <p>{projects.category}</p>
+                </div>
                 <img alt={projects.title} src={projectImage} />
-                <div className="overlay">
-                  <div className="portfolio-item-meta">
-                    <h5>{projects.title}</h5>
-                    <p>{projects.category}</p>
-                  </div>
-                </div>
-                <div className="link-icon">
-                  <i className="fa fa-link"></i>
-                </div>
               </a>
             </div>
           </div>
@@ -30,7 +25,7 @@ class Portfolio extends Component {
       <section id="portfolio">
         <div className="row">
           <div className="twelve columns collapsed">
-            <h2>Some of My Projects</h2>
+            <h1>Some of My Projects</h1>
 
             <div
               id="portfolio-wrapper"
