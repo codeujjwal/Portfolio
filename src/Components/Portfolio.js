9a104483c43a55f3fef5d1bb77be7d96098d8f9e
ujjwal sharma <sharma.ujjwal@curebay.com>
2024-07-31 23:35:59 +0530

    crash fix

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -24,6 +24,7 @@ class Portfolio extends Component {
                 href={projects.url}
                 title={projects.title}
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 <img alt={projects.title} src={projectImage} />
                 <div className="portfolio-item-meta">
